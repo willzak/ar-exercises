9fe45238dcb885c52615bdeada3af86bdd3d7e54
William <williamzak0@gmail.com>
2020-11-10 00:27:57 +0000

    completed ex 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,22 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+mens_stores = Store.where(mens_apparel: true)
+
+puts "----- MENS STORES -----"
+for location in mens_stores
+  puts "NAME: #{location.name}, ANNUAL REVENUE: #{location.annual_revenue}"
+end
+
+womens_stores = Store.where(womens_apparel: true)
+
+puts "----- WOMENS STORES -----"
+for location in womens_stores
+  if location.annual_revenue < 1000000
+    puts "NAME: #{location.name}, ANNUAL REVENUE: #{location.annual_revenue}"
+  end
+end
